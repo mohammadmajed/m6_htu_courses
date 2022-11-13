d14544111f0c1bf7a88ce03c8e97f05e025c4639
mohammadmajed <62719469+mohammadmajed@users.noreply.github.com>
2022-11-13 10:04:30 +0200

    Add files via upload

---
+++ htu_courses/contact_us.php
@@ -0,0 +1,25 @@
+<?php require './header.php';
+
+
+?>
+<main class="container my-5">
+
+<h1 class="  d-flex justify-content-center">Contact Us</h1>
+<hr class="w-50 m-auto">
+<br>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  <label for="exampleFormControlInput1" class="form-label">Phone number</label>
+  <input type="phone" class="form-control" id="exampleFormControlInput1" placeholder="">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <button class="btn btn-primary mt-2">Submit</button>
+</div>
+
+    </div>
+</main>
+<?php require './footer.php';
+?>
